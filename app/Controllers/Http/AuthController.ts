3793f4f143353be179f708997ed56417e6b95bd3
Marshall-Mathey <matheymarshall@gmail.com>
2023-10-09 20:39:10 +0000

    AuthController updated, login method added.

--- app/Controllers/Http/AuthController.ts
+++ app/Controllers/Http/AuthController.ts
@@ -6,7 +6,17 @@ export default class AuthController {
   public async register ({ request, response, auth }: HttpContextContract) {
     const payload = await request.validate(RegisterValidator)
     const user = User.create(payload)
-    const token: string = await auth.login(user)
+    const token = await auth.login(user)
     return token
+  }
+
+  public async login ({ request, response, auth }: HttpContextContract) {
+    const { email, password } = request.all()
+    try {
+      const token = await auth.attempt(email, password)
+      return token
+    } catch (error) {
+      return response.badRequest('Invalid credentials')
+    }
   }
 }
