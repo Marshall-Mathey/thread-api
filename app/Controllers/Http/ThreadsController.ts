c26354a78c77597bb94413452218ecbbd3e9d605
Marshall-Mathey <matheymarshall@gmail.com>
2023-10-10 06:54:36 +0000

    Update: sort_by & sort_order added

--- app/Controllers/Http/ThreadsController.ts
+++ app/Controllers/Http/ThreadsController.ts
@@ -2,6 +2,7 @@ import type { HttpContextContract } from '@ioc:Adonis/Core/HttpContext'
 import CreateThreadValidator from '../../Validators/CreateThreadValidator'
 import Thread from '../../Models/Thread'
 import UpdateThreadValidator from '../../Validators/UpdateThreadValidator'
+import SortValidator from '../../Validators/SortValidator'
 
 export default class ThreadsController {
   public async index ({ request, response }: HttpContextContract) {
@@ -10,9 +11,15 @@ export default class ThreadsController {
     const userId = request.input('user_id')
     const categoryId = request.input('category_id')
 
+    const payload = await request.validate(SortValidator)
+    // 
+    const sortBy = payload.sort_by || 'id'
+    const sortOrder = payload.sort_order || 'asc'
+
     const threads = await Thread.query()
       .if(userId, (query) => query.where('user_id', userId)).preload('user')
       .if(categoryId, (query) => query.where('category_id', categoryId))
+      .orderBy(sortBy, sortOrder)
       .preload('category')
       .preload('replies')
       .paginate(page, perPage)
